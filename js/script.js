b2dd5e098f30e8f2cac4927f277f806373ff4c9c
Frida Henriksen <fridahenriksen@hotmail.com>
2024-09-12 15:42:09 +0200

    Added comments to the js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
+//! Function to show the sidebar
 function showSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
 }
-
+//! Function to hide the sidebar
 function hideSidebar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
