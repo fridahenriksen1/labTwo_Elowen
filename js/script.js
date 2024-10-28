8da73818cb0788770b4051d619552f33fc1e4d2d
Frida Henriksen <fridahenriksen@hotmail.com>
2024-10-28 16:49:37 +0100

    Added another js file for the sidebar

--- js/script.js
+++ js/script.js
@@ -1,18 +1,11 @@
-//! Function to show the sidebar
-function showSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "flex";
-}
-//! Function to hide the sidebar
-function hideSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "none";
-}
+// ! Product data for the cards in index (array with products)
+let defineProductCards = [
+  { imageUrl1: "img/cutFlowers.jpg", description: "Snittblommor" },
+  { imageUrl1: "img/bridalBouquet.jpg", description: "Brudbukett" },
+  { imageUrl1: "img/pottedPlants.jpg", description: "Krukväxter" },
+  { imageUrl1: "img/GardenFlowers.jpg", description: "Trädgårdsblommor" },
+  { imageUrl1: "img/coffinDecoration.jpg" },
+  { imageUrl1: "img/mourningDecoration.jpg", description: "Sorgdekorationer" },
+];
 
-
-// let defineProductCards = [
-
-//   {imageUrl1: "/"}
-
-
-// ]
+console.log(defineProductCards);
---
+++ js/sidebar.js
@@ -0,0 +1,10 @@
+//! Function to show the sidebar
+function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+  }
+  //! Function to hide the sidebar
+  function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+  }
