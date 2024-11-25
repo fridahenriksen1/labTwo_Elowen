03224cd0898d437f9ab5fa5f06d13b0bad40841f
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-25 14:06:55 +0100

    Added descripton for an object

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let defineProductCards = [
   { imageUrl1: "img/bridalBouquet.jpg", description: "Brudbukett" },
   { imageUrl1: "img/pottedPlants.jpg", description: "Krukväxter" },
   { imageUrl1: "img/GardenFlowers.jpg", description: "Trädgårdsblommor" },
-  { imageUrl1: "img/coffinDecoration.jpg" },
+  { imageUrl1: "img/coffinDecoration.jpg", description: "Kistdekorationer" },
   { imageUrl1: "img/mourningDecoration.jpg", description: "Sorgdekorationer" },
 ];
 console.log(defineProductCards);
