4dfec6c344941c4b48ec92b7dd11d1195377edd5
Frida Henriksen <fridahenriksen@hotmail.com>
2024-12-05 16:42:49 +0100

    Saw I was thinking wrong and needed to get the removIcone when the content was showing

--- js/collapsible.js
+++ js/collapsible.js
@@ -1,14 +1,23 @@
+//! Collapsible for the product page
 const coll = document.getElementsByClassName("collapsible");
 let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    const content = this.nextElementSibling;
+    const removeIcon = this.querySelector(".remove-icon");
+    const addIcon = this.querySelector(".add-icon");
     if (content.style.maxHeight) {
+      //* show addIcon
       content.style.maxHeight = null;
+      removeIcon.style.display = "none";
+      addIcon.style.display = "block";
     } else {
+      //* show removeIcon
       content.style.maxHeight = content.scrollHeight + "px";
+      removeIcon.style.display = "block";
+      addIcon.style.display = "none";
     }
   });
 }
